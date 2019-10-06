48ea0d138c9a455827c2377936f0fd27289cd408
Diana M <dmmiller85@gmail.com>
2019-10-06 21:30:08 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,9 +9,11 @@ def second_challenge
     :capulet => {
       :patriarch => {}
       :matriarch => {}
-      :hero => {}
-      :hero_friends => {}
-      }}
+      :heroine => {}
+      :heroine_friends => {}
+      }
+    
+  }
 
   
 end
